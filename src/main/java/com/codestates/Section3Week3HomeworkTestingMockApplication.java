dfec60c4e0bf2c3ed3cf053342de148e67c8ee8e
hjs6877 <hjs6877@gmail.com>
2022-12-12 09:45:08 +0900

    .

--- src/main/java/com/codestates/Section3Week3HomeworkTestingMockApplication.java
+++ src/main/java/com/codestates/Section3Week3HomeworkTestingMockApplication.java
@@ -6,10 +6,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class Be39Section3Week3HomeworkTestingMockApplication {
+public class Section3Week3HomeworkTestingMockApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Be39Section3Week3HomeworkTestingMockApplication.class, args);
+		SpringApplication.run(Section3Week3HomeworkTestingMockApplication.class, args);
 	}
 
 }
--- src/test/java/com/codestates/slice/controller/member/MemberControllerTest.java
+++ src/test/java/com/codestates/slice/controller/member/MemberControllerTest.java
@@ -1,44 +1,39 @@
-package com.codestates.slice.mock;
+package com.codestates.slice.controller.member;
 
 import com.codestates.member.dto.MemberDto;
-import com.codestates.member.entity.Member;
-import com.codestates.member.mapper.MemberMapper;
-import com.codestates.member.service.MemberService;
-import com.codestates.stamp.Stamp;
 import com.google.gson.Gson;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.startsWith;
-import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+//    @WebMvcTest(controllers = MemberController.class)
+//    @MockBean(JpaMetamodelMappingContext.class)
+@Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
-class MemberControllerMockTest {
+class MemberControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private Gson gson;
 
-    @MockBean
-    private MemberService memberService;
+//    @MockBean
+//    private MemberService memberService;
 
-    @Autowired
-    private MemberMapper mapper;
-
+//    @MockBean
+//    private MemberMapper mapper;
 
     @Test
     void postMemberTest() throws Exception {
@@ -46,14 +41,6 @@ class MemberControllerMockTest {
         MemberDto.Post post = new MemberDto.Post("hgd@gmail.com",
                                                         "홍길동",
                                                     "010-1234-5678");
-
-        Member member = mapper.memberPostToMember(post);
-        member.setMemberId(1L);
-        member.setStamp(new Stamp());
-
-        // stubbing
-        given(memberService.createMember(Mockito.any(Member.class))).willReturn(member);
-
         String content = gson.toJson(post);
 
 
@@ -70,5 +57,33 @@ class MemberControllerMockTest {
         actions
                 .andExpect(status().isCreated())
                 .andExpect(header().string("Location", is(startsWith("/v11/members/"))));
+    }
+
+    @Test
+    void getMemberTest() throws Exception {
+        // given: MemberController의 getMember()를 테스트하기 위해서 postMember()를 이용해 테스트 데이터를 생성 후, DB에 저장
+        MemberDto.Post post = new MemberDto.Post("hgd@gmail.com","홍길동","010-1111-1111");
+        String postContent = gson.toJson(post);
+
+        ResultActions postActions =
+                mockMvc.perform(
+                        post("/v11/members")
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(postContent)
+                );
+        long memberId;
+        String location = postActions.andReturn().getResponse().getHeader("Location"); // "/v11/members/1"
+        memberId = Long.parseLong(location.substring(location.lastIndexOf("/") + 1));
+
+        // when / then
+        mockMvc.perform(
+                        get("/v11/members/" + memberId)
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.email").value(post.getEmail()))
+                .andExpect(jsonPath("$.data.name").value(post.getName()))
+                .andExpect(jsonPath("$.data.phone").value(post.getPhone()));
     }
 }
